24b5ba6155c755684f4e38dff1c9542e99830773
MoskovchenkoDmytro-KSUAm-20-1 <d.moskowchenko@gmai.com>
2022-01-25 23:44:42 +0200

    Added a DataLoader bootstraping class (placeholder). Fixed ServiceMap classes. Closes #14

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,53 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.model.Owner;
+import guru.springframework.model.Vet;
+import guru.springframework.services.OwnerService;
+import guru.springframework.services.VetService;
+import guru.springframework.services.map.OwnerServiceMap;
+import guru.springframework.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    //TODO: refactor it
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    // This method will run on Spring Context load
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Owner 1 FN");
+        owner1.setLastName("Owner 1 LN");
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Owner 2 FN");
+        owner2.setLastName("Owner 2 LN");
+        ownerService.save(owner2);
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Vet 1 FN");
+        vet1.setLastName("Vet 1 LN");
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(1L);
+        vet2.setFirstName("Vet 2 FN");
+        vet2.setLastName("Vet 2 LN");
+        vetService.save(vet2);
+
+        System.out.println("DetaLoader - run() over");
+    }
+}
